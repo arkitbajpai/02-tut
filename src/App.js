fa9d8626d5b83493afd4a7a5da21ffd96e5da4bb
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-08-25 22:30:13 +0530

    Implement Form and Button components; update App to use Form for user input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import From from './Form';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/";
+  const [req,type,setReqType] = useState("users");
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+   const fetchItems = async () => {
+    try{
+    const resposne = await fetch(`${API_URL}${reqType}`);
+    const data= await resposne.json();
+    setItems(data);
+    }catch(err){
+      console.error(err); 
+    }
+   }
+    fetchItems();
+  },{reqType})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <From reqType={reqType} setReqType={setReqType}/>
     </div>
   );
 }
